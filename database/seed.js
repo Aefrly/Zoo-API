b26f67a428de353ba0747604ac2a8290777d3eeb
Aefrly <fraley.allison@ic.edu>
2026-04-08 13:41:03 -0500

    Beginning information for my seed file

---
+++ database/seed.js
@@ -0,0 +1,8 @@
+const { db, Zoo, Animal, Attraction, Zookeeper, User} = require('./setup');
+
+//Initialize Database and Sync Models
+async function initializeDatabase() {
+    
+}
+
+initializeDatabase();
